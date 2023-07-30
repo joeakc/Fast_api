c5e5482bf984594f163963b6816536a87890845f
joe.abi.khalil1 <joe.abi.khalil1@gmail.com>
2023-07-30 17:58:53 +0200

    feat: test the post and get with postman

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print ('Hello')
+from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
+
+
+class Post(BaseModel):
+    title :str
+    content: str
+    published: bool = True
+    id: int
+    rating: Optional[int] =None
+
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message":"Welcome to my API"}
+
+@app.get("/posts")
+def get_posts():
+    return {"message": "These are my posts"}
+
+@app.post("/createposts")
+def create_posts(post: Post):
+    print (post)
+    print (post.model_dump())
+    return {"New post":post}
